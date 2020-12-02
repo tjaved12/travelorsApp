eee8ec6af5a752cc80efa64dd7382f72ddbbc432
ksbrazier <kylebrazier@gmail.com>
2020-12-01 20:29:40 -0500

    api route

---
+++ src/components/Selection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SelectCountry({ changeMood }) {
+  return (
+    <div>
+    Select The country you want to Visit!!
+    </div>
+  );
+}
+
+export default SelectCountry;
---
+++ src/pages/Selection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Selection() {
+  return (
+    <div>
+    Select The country you want to Visit!!
+    </div>
+  );
+}
+
+export default Selection;
