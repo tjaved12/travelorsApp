63c21fecc29258086e252dd992fa9dc695ae83be
TJ <tahmeenaowais@yahoo.com>
2020-12-03 19:06:37 -0500

    url fixed

--- client/src/pages/Convertor.js
+++ client/src/pages/Convertor.js
@@ -1,65 +1,35 @@
-import React,{useState} from "react";
-import API from "../utils/API";
-import {useHistory} from 'react-router-dom';
+import React, {Component} from "react";
+import API from "../utils/API"
+import {useHistory} from 'react-router-dom'
+import axios from "axios";
 
+class Convertor extends Component {
+  state = {
+    countryName:"",
+   
+  };
 
-function Convertor() {
-//   const [formState, setFormState] = useState({email: "", password: ""});
-
-//   const history = useHistory()
-//   const handleChange =(event) =>{
-//     // console.log(event.target, "TEST");
-//     const {name,value}=event.target;
-//     setFormState ({
-//       ...formState, 
-//       [name]: value
-//     })
-
-//   }
-//   const  handleSubmit = (event) => {
-//     event.preventDefault();
-//     // console.log(event, "COOL")
-    
-//     API.postlogin(
-//       {email: formState.email,
-//          password: formState.password
-//       }
-//     ).then(res => {
-//       console.log(res);
-//       history.push("/Selection")
-      
-//     })
   
-
-return (
-  <div>
-      <h2>Currency Convertor</h2>
-  
-  </div>
-);
-}
-export default Convertor;
-
+  componentDidMount() {
+   
+ axios.get("http://api.currencylayer.com/live?access_key=d738433b43524d560f4079256a7d254e&format=1")
+    .then(res => {
+       console.log(res)
+    });
+ }
+   render() {
 
 
-
-
-//  componentDidMount() {
-//  axios.get("http://api.currencylayer.com/live?access_key=d738433b43524d560f4079256a7d254e&format=1")
-//     .then(res => {
-//        console.log(res)
-//    render() {
-
-
-//     return (
-//       <div>
+    return (
+      <div>
      
-// <h4>Currency Convertor</h4>
-//       </div>
-//     )
+<h4>Currency Convertor</h4>
+      </div>
+    )
   
     
-//     }}
-//     )};
+    }
+    };
 
+    export default Convertor;
 
