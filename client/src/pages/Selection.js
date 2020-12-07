f72914b65ed3146949984ed0bc1bab607e207e1f
madhatter <matthew.rikard@gmail.com>
2020-12-06 21:26:08 -0500

    Selection.js styles updated to match login.js. not all countries visible or scrollable yet.

--- client/src/pages/Selection.js
+++ client/src/pages/Selection.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Card } from "react-bootstrap"
 import { CardDeck } from "react-bootstrap"
 import {useHistory} from 'react-router-dom'
+import classes from './BackgroundVideo.module.css';
+import "./Selection.css"
 
 function Selection() {
   const history = useHistory()
@@ -15,7 +17,10 @@ function Selection() {
   }
 
   return (
-        <div className="Wrapper">
+    <div className={classes.Container}>
+      <img src="https://lh3.googleusercontent.com/2NWt-FYrIy56hxHyJ8Ga7xP5KA1uTKL-C-zE6ebp_a-V0hEieIcO5Tpfp9nWCjlhKSjH6RJ_cA2HpiG231aIyxQ-7B9EI-NwywCEJHlJI0F1VjE9ISUInE6V-N6-YKAMLeMoG29rgvdz__9b3VPndN7y5EeLK0jNTEpQ_ttUSqYnybz1_YbEWMl7Z-GDly00uIWf64DOVgU3wMNhjSRO6XozcddE4jHJ6-6cpg1SWUmYH-P5H94BKS5R_OTklnP3U1Nxj-TFuFL8QRcUW7GFXQSGNnD_dzl58gH7nIJzDWJlWFTy2XP4Nu2kyVfqDiRbHsQy-pvOmUrEMMvzxz-4HUeqQyQUzQSysOFSPIBb9z41EIeTfAkQPsBfmsB9IdKBd55moujXao5NGGGA0Jdq1B5JmhJUGfeKydbZuZbSlOH1IwrAeaBwoyG_nW0wpQ_hS2KJX5tfnwuLrQ2yaewuurdv7jYrzMCay1Dc9qmev79IcC7bvRSWEHhAZeq0-3FDxflJngGVfuhEAiOYtv8qGyr3uiSOy-qT6JWMdCrhlGGMryFPqYkH_-It9GIzohy49WEnoL8EvJM0cBgou2dW7JFb1P9ZHKUd4Rw9b-JnahbgOwLc0jDQ4ZjhGZawSzxLD9BGNqFT2-pb0shkod-oIvgJBY07u7d0-skmk94BCFKLlhaP14pbbsq9VDl2sWU=w956-h717-no?authuser=0" alt="travel" />
+      <div className={classes.Content}>
+        <div className={classes.SubContent}>
           <CardDeck>
             <Card type="submit" className="country" onClick={() => {handleSubmit("Argentina")}}>
               <Card.Img variant="top" src="./pictures/argentina01.jpeg" />
@@ -121,7 +126,7 @@ function Selection() {
             </Card>
           </CardDeck>
           <CardDeck>
-           <Card type="submit" className="country" onClick={() => {handleSubmit("Netherlands")}}>
+          <Card type="submit" className="country" onClick={() => {handleSubmit("Netherlands")}}>
               <Card.Img variant="top" src="./pictures/netherlands01.jpeg" />
               <Card.Body>
                 <Card.Title>Netherlands</Card.Title>
@@ -147,6 +152,8 @@ function Selection() {
             </Card>
           </CardDeck>
         </div>
+      </div>
+    </div>
   )
 }
 
